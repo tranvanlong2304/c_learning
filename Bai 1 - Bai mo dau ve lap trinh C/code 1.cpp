2a2bea4e6ec0d2aa96db5306a6e75928471ed08f
Tran Van Long <git230407@gmail.com>
2025-08-26 23:52:27 +0700

    bai1

---
+++ Bai 1 - Bai mo dau ve lap trinh C/code 1.cpp
@@ -0,0 +1,15 @@
+//Hu?ng d?n s? d?ng Dev-C++ và cách ch?y chuong trình ngôn ng? C
+
+//#include <stdio.h>
+//int main(){
+//	printf("tung ngu lon !");
+//	 return 1;
+//}
+
+
+// Các ki?u d? li?u chính và d?c t? c?a chúng
+
+
+
+
+//Bi?n trong ngôn ng? C, các quy t?c d?t tên bi?n
