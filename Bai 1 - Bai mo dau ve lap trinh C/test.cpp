a1db4b19542f72f76ea802d8f81cad5acd7d0d7b
Tran Van Long <git230407@gmail.com>
2025-09-07 21:22:14 +0700

    update

---
+++ Bai 1 - Bai mo dau ve lap trinh C/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Kich thuoc int: " << sizeof(int) * 8 << " bit" << endl;
+    cout << "Kich thuoc float: " << sizeof(float) * 8 << " bit" << endl;
+    return 0;
+}
+
